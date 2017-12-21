02bc541bc7c5d73928421adabac85dbd0a574584
Mark Pauley <pauley@unsaturated.net>
2017-12-20 21:46:30 -0800

    Day 16, Part 2

--- 16/permutation_promenade.c
+++ 16/permutation_promenade.c
@@ -16,13 +16,37 @@ char programs[] = "abcdefghijklmnop";
 //char programs[] = "abcde";
 char* prog_ptr = programs;
 
+char output[NUM_PROGRAMS+1];
+
+typedef struct {
+	long unsigned hash;
+	long unsigned result_hash;
+	char input[NUM_PROGRAMS+1];
+	char result[NUM_PROGRAMS+1];
+} Mem;
+
+#define MEMORY_SIZE 4096
+Mem memory[MEMORY_SIZE];
+
+#define wrap(i,len) (((i)>=0)?((i) % (len)):((len)-((-(i))%(len))))
 
 /* Commands */
 #define SPIN 's'
 #define EXCHANGE 'x'
 #define PARTNER 'p'
 
-#define wrap(i,len) (((i)>=0)?((i) % (len)):((len)-((-(i))%(len))))
+typedef struct {
+	enum { spin, exchange, partner } type;
+	union {
+		int spin_val;
+		struct { int a; int b; } exchange_val;
+		struct { char a; char b; } partner_val;
+	};
+} Command;
+
+#define NUM_COMMANDS 12000
+Command commands[NUM_COMMANDS];
+size_t num_commands = 0;
 
 /* Parsers */
 
@@ -40,7 +64,7 @@ void parse_partner(FILE* in, char* a, char* b) {
 	fscanf(in, "%c/%c", a, b);
 }
 
-int find_idx(char name) {
+static inline int find_idx(char name) {
 	int offset = prog_ptr - programs;
 	for( int i = 0; i < NUM_PROGRAMS; i++) {
 		if (programs[i] == name) {
@@ -51,7 +75,7 @@ int find_idx(char name) {
 	return -1;
 }
 
-void do_spin(int num) {
+static inline void do_spin(int num) {
 	assert(num < NUM_PROGRAMS);
 	assert(num >= 0);
 	int offset = prog_ptr - programs;
@@ -62,7 +86,7 @@ void do_spin(int num) {
 	assert(prog_ptr < programs + NUM_PROGRAMS);
 }
 
-void do_exchange(int a, int b) {
+static inline void do_exchange(int a, int b) {
 	assert(a >= 0 && a < NUM_PROGRAMS);
 	assert(b >= 0 && b < NUM_PROGRAMS);
 	int offset = (int)(prog_ptr - programs);
@@ -73,8 +97,69 @@ void do_exchange(int a, int b) {
 	programs[offset_b] = temp;
 }
 
-void do_partner(char a, char b) {
+static inline void do_partner(char a, char b) {
 	do_exchange(find_idx(a), find_idx(b));
+}
+
+void do_command(const Command* command) {
+	switch(command->type) {
+	case spin:
+		do_spin(command->spin_val);
+		break;
+	case exchange:
+		do_exchange(command->exchange_val.a, command->exchange_val.b);
+		break;
+	case partner:
+		do_partner(command->partner_val.a, command->partner_val.b);
+		break;
+	}
+}
+
+// Turns out that hash is a pretty useful
+// function
+unsigned long hash(const char *str) {
+	unsigned long hash = 5381;
+	int c;
+	
+	while (*str) {
+		c = *str++;
+		/* hash * 33 + c */
+		hash = ((hash << 5) + hash) + c; 
+	}
+	
+	return hash;
+}
+
+Mem* find_mem(char* input, unsigned long in_hash) {
+	if (!in_hash) {
+		in_hash = hash(input);
+	}
+	Mem* result = &memory[in_hash % MEMORY_SIZE];
+	while(result->hash && result->hash != in_hash) {
+		result++;
+		if (result - memory > MEMORY_SIZE)result = memory;
+	}
+	if (result->hash == in_hash) {
+		return result;
+	}
+	return NULL;
+}
+
+Mem* insert_mem(char* input, char* output) {
+	unsigned long in_hash = hash(input);
+	unsigned long out_hash = hash(output);
+
+	Mem* result = &memory[in_hash % MEMORY_SIZE];
+	while(result->hash) {
+		result++;
+		if (result - memory > MEMORY_SIZE)result = memory;
+	}
+
+	result->hash = in_hash;
+	result->result_hash = out_hash;
+	strcpy(result->input, input);
+	strcpy(result->result, output);
+	return result;
 }
 
 void print_programs(FILE* out) {
@@ -82,6 +167,14 @@ void print_programs(FILE* out) {
 	for (int i = 0; i < NUM_PROGRAMS; i++) {
 		putc(programs[wrap(i + offset, NUM_PROGRAMS)], out);
 	}
+}
+
+void render_programs(char outBuf[NUM_PROGRAMS+1]) {
+	int offset = prog_ptr - programs;
+	for (int i = 0; i < NUM_PROGRAMS; i++) {
+		outBuf[i] = programs[(i + offset) % NUM_PROGRAMS];
+	}
+	outBuf[NUM_PROGRAMS] = '\0';
 }
 
 int main(int argc, char** argv) {
@@ -99,26 +192,29 @@ int main(int argc, char** argv) {
 	while(EOF != (c = getc(in))) {
 		switch(c) {
 		case SPIN: {
-			int spin = parse_spin(in);
-			printf("s%d: ", spin);
-			assert(spin >= 0);
-			do_spin(spin);
+			int spin_val = parse_spin(in);
+			assert(spin_val >= 0);
+			commands[num_commands++] = (Command){
+				.type = spin, .spin_val = spin_val
+			};
 		}
 			break;
 
 		case EXCHANGE: {
 			int a, b;
 			parse_exchange(in, &a, &b);
-			printf("x%d/%d: ", a, b);
-			do_exchange(a, b);
+			commands[num_commands++] = (Command){
+				.type = exchange, .exchange_val = { a, b }
+			};
 		}
 			break;
 
 		case PARTNER: {
 			char a, b;
 			parse_partner(in, &a, &b);
-			printf("p%c/%c: ", a, b);
-			do_partner(a, b);
+			commands[num_commands++] = (Command){
+				.type = partner, .partner_val = { a, b }
+			};
 		}
 			break;
 
@@ -130,10 +226,53 @@ int main(int argc, char** argv) {
 			fprintf(stderr, "Unexpected char %c\n", c);
 			return -1;
 		}
-		print_programs(stdout);
-		printf("\n");
+	}
+	printf("Read %zu commands\n", num_commands);
+	
+	for (size_t i = 0; i < num_commands; i++) {
+		do_command(commands + i);
 	}
 	printf("Part 1: ");
+	print_programs(stdout);
+	printf("\n");
+
+	printf("Part 2: ");
+	
+	render_programs(output);
+	/*
+	 * Please excuse the mess, I had to hastily write a memoization
+	 * scheme without a proper hash table :-/
+	 */
+	bzero(memory, sizeof(Mem) * MEMORY_SIZE);
+	Mem* mem = insert_mem("abcdefghijklmnop", output);
+	for (size_t j = 1; j < 1000000000UL; j++) {
+		assert(mem);
+		Mem* next = find_mem(mem->result, mem->result_hash);
+		if (!next) {
+			strcpy(programs, mem->result);
+			prog_ptr = programs;
+			for (size_t i = 0; i < num_commands; i++) {
+				do_command(commands + i);
+			}
+			render_programs(output);
+			next = insert_mem(mem->result, output);
+			/*
+			printf("%s(%lu) -> %s(%lu)\n", next->input, next->hash,
+						 next->result, next->result_hash);
+			*/
+		}
+		assert(next);
+		mem = next;
+		
+		if ((j % 1000000) == 0) {
+			putc('.', stdout);
+			fflush(stdout);
+		}
+	}
+	strcpy(programs, mem->result);
+	prog_ptr = programs;
+	
+	printf("\n");
 	print_programs(stdout);
 	printf("\n");
 	
